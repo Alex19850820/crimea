bd4b990d5830bd14d353f29abe52c8e48056caac
Александр <shlykovn@mail.ru>
2019-02-18 17:15:44 +0300

    yandex

--- wp-content/themes/crimea/header.php
+++ wp-content/themes/crimea/header.php
@@ -18,7 +18,7 @@ $contacts = fw_get_db_customizer_option();
 	<meta name="viewport" content="width=device-width height=device-height initial-scale=1.0 maximum-scale=1.0 user-scalable=0">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Work+Sans:300,700,800%7COswald:300,400,500">
-
+	<meta name="yandex-verification" content="5276ff3faf383b65" />
 	<?php wp_head(); ?>
 </head>
 
